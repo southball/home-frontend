ad03370e316022f2ac30093bc5dd72e77e570c62
Southball <jamie.choi.mail.2002@gmail.com>
2020-02-05 02:18:28 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
+};
